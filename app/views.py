07b4c6083b65e0efe87cff6843379471436aec5d
natafaye <nata.faye@gmail.com>
2014-04-02 16:39:32 -0600

    Added templates

--- app/views.py
+++ app/views.py
@@ -1,6 +1,24 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-	return "Hello, World!"
+	return render_template("home.html", title = "Home")
+	
+@app.route('/student-signup')
+def studentsignup():
+	return render_template("home.html", title = "Sign Up")
+	
+@app.route('/submissions')
+def submissions():
+	return render_template("home.html", title = "Submissions")
+	
+@app.route('/past-assignments')
+def past_assignments():
+	return render_template("home.html", title = "Past Assignments")
+
+@app.route('/current-assignments')
+def current_assignment():
+	return render_template("home.html", title = "Current Assignments")
+	
