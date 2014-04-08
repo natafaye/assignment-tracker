78d4f6fc6aa3f44179352db0bb3b52dff8993280
drmcarthur <mcarthur.dr@gmail.com>
2014-04-07 22:08:41 -0600

    Added view routing to all pages
    Added routes to all 10 pages in views.py. Also added flask's template
    functions to all of the html pages so that they are generated properly.

--- app/views.py
+++ app/views.py
@@ -6,19 +6,42 @@ from app import app
 def index():
 	return render_template("home.html", title = "Home")
 	
+@app.route('/classes')
+def classes():
+	return render_template("classes.html", title = "Classes")
+
+@app.route('/create-assignment')
+def create_assignment():
+	return render_template("create-assignment.html", title = "Create Assignment")
+
+@app.route('/create-class')
+def create_class():
+	return render_template("create-class.html", title = "Create Class")
+	
+@app.route('/current-assignments')
+def current_assignment():
+	return render_template("current-assignments.html", title = "Current Assignments")
+
+@app.route('/home-not-logged-in')
+def home_not_logged_in():
+	return render_template("home-not-logged-in.html", title = "Sign Up")
+
+@app.route('/home-student')
+def home_student():
+	return render_template("home-student.html", title = "Home")
+
+@app.route('/past-assignments')
+def past_assignments():
+	return render_template("past-assignments.html", title = "Past Assignments")
+
 @app.route('/student-signup')
-def studentsignup():
-	return render_template("home.html", title = "Sign Up")
+def student_signup():
+	return render_template("student-signup.html", title = "Sign Up")
 	
 @app.route('/submissions')
 def submissions():
-	return render_template("home.html", title = "Submissions")
-	
-@app.route('/past-assignments')
-def past_assignments():
-	return render_template("home.html", title = "Past Assignments")
+	return render_template("submissions.html", title = "Submissions")
 
-@app.route('/current-assignments')
-def current_assignment():
-	return render_template("home.html", title = "Current Assignments")
-	
+@app.route('/submit-assignment')
+def submit_assignment():
+	return render_template("submit-assignment.html", title = "Submit Assignment")
