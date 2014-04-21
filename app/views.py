69d10c0ccdfaf142ef915eb39210873edf60de65
drmcarthur <mcarthur.dr@gmail.com>
2014-04-20 18:29:47 -0600

    Database can now add classes and assignments
    Linked the create assignment and create class pages to the database.
    Added modals to report on the success/failure of the create-class and
    create-assignment functions. Added a consistent format string for
    displaying due dates. Modified the safe_division function to return 0
    when dividing by 0 (so that if there are no students in a particular
    class, the current-assignments page will display 0% submitted and
    refrain from creating a "View" button.

--- app/views.py
+++ app/views.py
@@ -1,8 +1,13 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask.ext.login import login_user, logout_user, current_user, login_required
+from flask import jsonify
 from app import app, db, lm, oid
 from forms import LoginForm
-from models import User, Assignment, Submission, ROLE_STUDENT, ROLE_INSTRUCTOR
+from models import User, Assignment, Submission, ROLE_STUDENT, ROLE_INSTRUCTOR, Course
+import string, random, datetime
+
+# Make one date format to be consistent throughout the pages
+dateFMT = "%a. %B %d, %Y"
 
 ############### Home and Unauthenticated Pages ##############
 
@@ -43,7 +48,7 @@ def current_assignments():
 	# Sort assignments by due date
 	assignments.sort(key=lambda assignment:assignment["dueDate"]);
 	
-	return render_template("current-assignments.html", title = "Current Assignments", user=g.user, assignments=assignments)
+	return render_template("current-assignments.html", title = "Current Assignments", user=g.user,assignments=assignments,dateFMT=dateFMT)
 	
 @app.route('/past-assignments')
 @login_required
@@ -56,7 +61,7 @@ def past_assignments():
 	# Sort assignments by due date
 	assignments.sort(key=lambda assignment:assignment["dueDate"]);
 	
-	return render_template("past-assignments.html", title = "Past Assignments", user=g.user, assignments=assignments)
+	return render_template("past-assignments.html", title = "Past Assignments", user=g.user, assignments=assignments,dateFMT=dateFMT)
 	
 @app.route('/classes')
 @login_required
@@ -103,6 +108,45 @@ def create_class():
 	
 	return render_template("create-class.html", title = "Create Class", user=g.user)
 
+############## Front-end Database requests ##################
+@app.route('/create-class-post', methods = ['POST'])
+@login_required
+def add_class_to_db():
+	# Set up course code generator & retrieve form data
+	course_code = generate_course_code()
+	course_name = request.form['name']
+	
+	# Add to database
+	add_course_for_instructor(course_name,course_code)
+	
+	# Send response
+	return jsonify({
+		'name':course_name,
+		'code':course_code })
+
+@app.route('/create-assignment-post', methods = ['POST'])
+@login_required
+def add_assignment_to_db():
+	# Retrieve form data
+	course_code = request.form['class_code']
+	assign_name = request.form['name']
+	instructions = request.form['instr']
+	datestr = request.form['dueDate']
+	
+	# Format Date
+	dueDate = datetime.datetime.strptime(datestr, "%Y-%m-%d")
+	returnDate = dueDate.strftime(dateFMT)
+
+	# Add to database
+	add_assignment_for_course(assign_name,course_code,instructions,dueDate)
+	
+	# Send response
+	return jsonify({
+		'course':course_code,
+		'name':assign_name,
+		'instr':instructions,
+		'due': returnDate})
+
 
 ####################### Student Pages #######################
 
@@ -114,7 +158,7 @@ def home_student():
 		
 	course = get_course_for_student()
 	
-	return render_template("home-student.html", title = course["name"], user=g.user, course=course)
+	return render_template("home-student.html", title = course["name"], user=g.user, course=course,dateFMT=dateFMT)
 
 @app.route('/submit-assignment/<assignment_id>')
 @login_required
@@ -182,6 +226,30 @@ def logout():
 
 ################## Database Functions #################
 		
+def add_course_for_instructor(name,course_code):
+	# Create the new course
+	newCourse = Course(title=name,code=course_code)
+	db.session.add(newCourse)
+	db.session.commit()
+	
+	# Add the course the instructor's list of courses
+	g.user.courses.append(newCourse)
+	db.session.commit()
+	return
+
+def delete_course(course_code):
+	delCourse = Course.query.filter_by(code=course_code).first()
+	db.session.delete(delCourse)
+	db.session.commit()
+	return
+
+def add_assignment_for_course(name,course_code,descr,dueDate):
+	theCourse = Course.query.filter_by(code=course_code).first()
+	newAssignment = Assignment(title=name,description=descr,due_date=dueDate,course=theCourse)
+	db.session.add(newAssignment)
+	db.session.commit()
+	return
+	
 def get_courses_for_instructor():
 	# Check permissions
 	if not g.user.is_authenticated():
@@ -207,7 +275,7 @@ def get_course_for_student():
 		"assignments": []
 	}
 	for a in g.user.course.assignments:
-		course.assignments.append({
+		course['assignments'].append({
 			"name": a.title,
 			"dueDate": a.due_date,
 			"submitted": get_date_submitted(a, g.user)
@@ -283,9 +351,20 @@ def get_date_submitted(a, u):
 		return ""
 		
 def get_percentage(a):
-	return str(safe_division(a.submissions.count(), len(a.course.students)) * 100) + "%"
+	return safe_division(a.submissions.count(), len(a.course.students)) * 100
 
 def safe_division(dividend, divisor):
 	if divisor == 0:
-		return 1
+		return 0
 	return dividend / divisor
+
+def generate_course_code():
+	size = 7
+	chars = string.ascii_uppercase + string.digits
+	newCode = ''.join(random.choice(chars) for _ in range(size))
+	
+	# Require all IDs to have at least one digit
+	while(any(char.isdigit() for char in newCode) != True):
+		newCode = ''.join(random.choice(chars) for _ in range(size))
+
+	return newCode
